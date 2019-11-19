51df68c6113e103d3700a968f1d2bca1983e1e6d
your <vutran6853@gmail.com>
2019-11-18 19:16:30 -0600

    add eslint rule disable some and add some

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from '../HelloWorld/HelloWorld'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
